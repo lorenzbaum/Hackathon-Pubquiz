d6b36ed284e2d00268645fe2b6c265c80df3d135
David Schäfer <david.schaefer@analytical-software.de>
2024-01-17 17:43:32 +0100

    fix wikipedia_tool

--- wikipedia_tool.py
+++ wikipedia_tool.py
@@ -1,11 +1,12 @@
 from langchain.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
 
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
 wikipedia_tool = Tool.from_function(
-    func = ddg.run,
+    func = wikipedia.run,
     name = "Wikipedia Search",
     description = "Search wikipedia encyclopedia for common questions.",
 )
