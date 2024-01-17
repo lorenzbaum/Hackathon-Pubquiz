0a29fb1044476cea39ea84eed11bb3ac2b6029b9
David Schäfer <david.schaefer@analytical-software.de>
2024-01-17 17:37:07 +0100

    improve description

--- wikipedia_tool.py
+++ wikipedia_tool.py
@@ -7,5 +7,5 @@ wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 ddg_tool = Tool.from_function(
     func = ddg.run,
     name = "Wikipedia Search",
-    description = "Search an encyclopedia for common questions.",
+    description = "Search wikipedia encyclopedia for common questions.",
 )
